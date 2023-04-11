831a10e6d203c1a417b39199e70de162b27208d8
javeriadarwaish <javeriadarwaish@gmail.com>
2023-04-11 00:22:03 -0700

    variable js repo

---
+++ app.js
@@ -0,0 +1,68 @@
+// --------------question 1-------------
+// Declare a variable called username.
+
+// let username;
+
+// ---------------question 2 -----------
+// Declare a variable called myName and assign to it a string that represents your full name..
+
+// let myName = "Javeria Darwaish";
+
+
+// -------------question 3-------------
+// write script to
+// a)Declare a JS variable,titled message.
+// b)Assign "Hello World" to variable message.
+// c)Display the message in alert box..
+
+// let message="Hello World";
+// alert(message);
+
+// ----------question 4----------------
+// Write a script to save student's bio data in JS variables and show the data in alert boxes..
+
+// let studentName = "Jhone Doe";
+// let studentAge = "15 years old";
+// let studentSkills = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentSkills);
+
+// ----------question 5-------------------
+// write a script to display the following alert using one JS variable:
+        //    PIZZA
+        //    PIZZ
+        //    PIZ
+        //    PI
+        //    P
+    
+//    let food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//    alert(food);   
+
+// ----------question 6-------------------
+// DEclare a variable called email and assign to it a string that represents your Email Address.
+// message show in alert box...
+
+// let email = "javeria@gmail.com";
+// alert("My email address is " +email);
+
+// ----------question 7------------------
+// Declare a variable called book and give it the value "A smarter way to learn Javascript".
+// Display the following message in an alert box:
+
+// let book = "A smarter way to learn Javascript";
+// alert("I am trying to learn from the book " +book);
+
+// ----------question 7---------------
+// write a script to display this in browser through JS...
+
+// let content = ("Yah! I can write HTML content through Javascript");
+// document.write(content);
+
+// ------------questio 8-------------
+// store following string in a variable and show in alert and browser through JS
+
+let assign = "___________ஜ۩۞۩ஜ______________";
+alert(assign);
+
